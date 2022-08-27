8b2bc1b9288950ee10fb797ab105377c9cfc89d4
Florencia Castro <florencia.castro@bibamgroup.com>
2022-08-26 21:32:42 -0300

    Configuración de datasource con properties y classes

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -4,15 +4,20 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentosplatzi.springboot.fundamentos.entity.User;
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import com.fundamentosplatzi.springboot.fundamentos.repository.UserRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
-import java.util.logging.Logger;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
@@ -24,16 +29,19 @@ public class FundamentosApplication implements CommandLineRunner {
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWithProperties myBeanWithProperties;
 	private UserPojo userPojo;
+	private UserRepository userRepository;
 
 
 	public FundamentosApplication(@Qualifier("componentTwoImpl") ComponentDependency componentDependency,
 								  MyBean myBean, MyBeanWithDependency myBeanWithDependency,
-								  MyBeanWithProperties myBeanWithProperties, UserPojo userPojo){
+								  MyBeanWithProperties myBeanWithProperties, UserPojo userPojo,
+								  UserRepository userRepository){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
 		this.userPojo = userPojo;
+		this.userRepository = userRepository;
 	}
 
 	public static void main(String[] args) {
@@ -42,6 +50,46 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args){
+		saveUserInDB();
+		getInformalJPQLFromUser();
+
+	}
+
+	private void getInformalJPQLFromUser(){
+		LOGGER.info("Usuario obtenido con el metodo findByUserEmail" +
+				userRepository.findMyUserByEmail("john@domain.com")
+						.orElseThrow(()-> new RuntimeException("no se encontro el usuario")));
+
+		userRepository.findAndSort("Test", Sort.by("id").descending())
+				.stream()
+				.forEach(user -> LOGGER.info("Info de usuario " + user));
+	}
+
+
+	private void saveUserInDB(){
+		User user1 = new User("John", "john@domain.com", LocalDate.of(2021, 03, 15));
+		User user2 = new User("Julie", "julie@domain.com", LocalDate.of(2021, 03, 20));
+		User user3 = new User("Daniela", "daniela@domain.com", LocalDate.of(2021, 03, 25));
+		User user4 = new User("Oscar", "oscar@domain.com", LocalDate.now());
+		User user5 = new User("Test1", "Test1@domain.com", LocalDate.now());
+		User user6 = new User("Test2", "Test2@domain.com", LocalDate.now());
+		User user7 = new User("Test3", "Test3@domain.com", LocalDate.now());
+		User user8 = new User("Test4", "Test4@domain.com", LocalDate.now());
+		User user9 = new User("Test5", "Test5@domain.com", LocalDate.now());
+		User user10 = new User("Test6", "Test6@domain.com", LocalDate.now());
+		User user11 = new User("Test7", "Test7@domain.com", LocalDate.now());
+		User user12 = new User("Test8", "Test8@domain.com", LocalDate.now());
+		User user13 = new User("Test9", "Test9@domain.com", LocalDate.now());
+		List<User> list = Arrays.asList(user1,user2,user3,user4, user5, user6, user7, user8, user9, user10, user11, user12, user13);
+		list.stream().forEach(userRepository::save);
+
+	}
+
+	private void delete(){
+		userRepository.deleteAll();
+	}
+
+	private void ejemplosAnteriores() {
 		componentDependency.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
